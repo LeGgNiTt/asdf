47d92c914d4ee830e8b5df1ace122ddfba7d4608
LeGgNiT <aleksandrs.unterkirhers@bluewin.ch>
2023-11-06 14:58:35 +0100

    added even more helpful fnctions

--- funcs.py
+++ funcs.py
@@ -1,5 +1,7 @@
 from models import *
-from json import jsonify
+from flask import jsonify
+
+
 
 def find_tutor_by_subject(subject_id):
     tutors = db.session.query(Tutor).join(TutorSubject).filter(TutorSubject.subject_id == subject_id).all()
@@ -19,3 +21,28 @@ def create_lesson(tutor_id, student_id, subject_id, date, start_time, end_time):
     db.session.commit()
 
     return jsonify({'message': 'Lesson created successfully!'})
+
+
+import datetime
+import calendar
+
+def generate_calendar(tutor_id):
+    # Get the current date
+    today = datetime.date.today()
+
+    # Get the first day of the current month
+    first_day = today.replace(day=1)
+
+    # Get the number of days in the current month
+    _, num_days = calendar.monthrange(today.year, today.month)
+
+    # Generate a list of dates for the current month
+    dates = [first_day + datetime.timedelta(days=i) for i in range(num_days)]
+
+    # Create a list of lessons for the current month for the given tutor_id
+    lessons = []
+    for date in dates:
+        lessons_for_date = check_lessons(tutor_id, date)
+        lessons.append({'date': date, 'lessons': lessons_for_date})
+
+    return jsonify({'tutor_id': tutor_id, 'month': today.month, 'year': today.year, 'lessons': lessons})
